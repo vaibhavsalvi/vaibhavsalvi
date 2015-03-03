b71accbab5609b2fdbfd8090cd9f530cc05d0d46
vaibhavsalvi <vaibhav.v.salvi@gmail.com>
2015-03-04 00:54:56 +0530

    Update

--- Closure.js
+++ Closure.js
@@ -1,12 +1,13 @@
 function AddToSeed( seedValue)
 {
-    alert(this + "In AddToSeed");
-    //alert(seedValue + " - SeedValue ");
+    
+    alert(seedValue + " - SeedValue " + this + "In AddToSeed");
     return function closure(addValue) {
         alert(this + "In Closure");
-       // alert(addValue + " - Adding to Seed");
+       
         seedValue = parseInt(seedValue) + parseInt(addValue);  // SeedValue will be changed in each subsequent closure invocation, unless it is not a self executing immediate function
-        //alert(" New Seed Value now is " + seedValue);
-    };
+        alert(" New Seed Value now is " + seedValue);
+   // }  Uncomment, to show non IMEI
+    }(seedValue);
      
 }
